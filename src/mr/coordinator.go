899b3a135ee2f95535bae7efd3a68e28ee01b09a
chengxiang <chengxiang085@gmail.com>
2024-04-16 10:38:41 +0800

    feat: implement Lab1: MapReduce

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -1,15 +1,22 @@
 package mr
 
-import "log"
+import (
+	"errors"
+	"log"
+	"sync"
+	"time"
+)
 import "net"
 import "os"
 import "net/rpc"
 import "net/http"
 
-
 type Coordinator struct {
 	// Your definitions here.
-
+	MapTasks    []Task
+	ReduceTasks []Task
+	Mrinfo      MRInfo
+	mtx         sync.Mutex
 }
 
 // Your code here -- RPC handlers for the worker to call.
@@ -24,6 +31,119 @@ func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
 	return nil
 }
 
+func (c *Coordinator) IsMapFinished() bool {
+	ret := true
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	for _, task := range c.MapTasks {
+		if task.TaskType == MapTask {
+			if task.TaskStatus != Finished {
+				ret = false
+			}
+		}
+	}
+
+	return ret
+}
+
+func (c *Coordinator) IsReduceFinished() bool {
+	ret := true
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+	for _, task := range c.ReduceTasks {
+		if task.TaskType == ReduceTask {
+			if task.TaskStatus != Finished {
+				ret = false
+			}
+		}
+	}
+
+	return ret
+}
+
+// Can be Map task or Reduce task
+func (c *Coordinator) GetUnassignedTask() Task {
+	c.mtx.Lock()
+	// Read MapTasks first, if all the map tasks are completed, then read ReduceTasks
+	for i, task := range c.MapTasks {
+		if task.TaskStatus == UnAssigned {
+			c.MapTasks[i].TaskStatus = Assigned
+			c.mtx.Unlock()
+			return task
+		}
+	}
+	c.mtx.Unlock()
+
+	if c.IsMapFinished() {
+		c.mtx.Lock()
+		for i, task := range c.ReduceTasks {
+			if task.TaskStatus == UnAssigned {
+				c.ReduceTasks[i].TaskStatus = Assigned
+				c.mtx.Unlock()
+				return task
+			}
+		}
+		c.mtx.Unlock()
+	}
+
+	return Task{TaskType: NoMoreTask}
+}
+
+// Assign a task to a worker
+func (c *Coordinator) AssignTask(args *ArgsType, reply *Task) error {
+	*reply = c.GetUnassignedTask()
+
+	if reply.TaskType == NoMoreTask {
+		return errors.New("NoMoreMapTaskOrReduceTask")
+		//return nil
+	} else {
+		timer := time.NewTimer(10 * time.Second)
+		// 10s 之后将没有完成的task重置为UnAssigned并重新参与task分配
+		go func(c *Coordinator, task *Task) {
+			<-timer.C
+			c.mtx.Lock()
+			defer c.mtx.Unlock()
+			if reply.TaskType == MapTask && c.MapTasks[reply.TaskIndex].TaskStatus == Assigned {
+				c.MapTasks[reply.TaskIndex].TaskStatus = UnAssigned
+			} else if reply.TaskType == ReduceTask && c.ReduceTasks[reply.TaskIndex].TaskStatus == Assigned {
+				c.ReduceTasks[reply.TaskIndex].TaskStatus = UnAssigned
+			}
+		}(c, reply)
+
+		return nil
+	}
+}
+
+// Update task status to Finished
+func (c *Coordinator) UpdateTaskStatus(args *TaskFinishArgs, reply *TaskFinishReply) error {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
+	if args.TaskType == MapTask {
+		for i, task := range c.MapTasks {
+			if task.TaskIndex == args.TaskId {
+				c.MapTasks[i].TaskStatus = Finished
+				break
+			}
+		}
+	} else if args.TaskType == ReduceTask {
+		for i, task := range c.ReduceTasks {
+			if task.TaskIndex == args.TaskId {
+				c.ReduceTasks[i].TaskStatus = Finished
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (c *Coordinator) GetMRInfo(args ArgsType, reply *MRInfo) error {
+	reply.MapNum = c.Mrinfo.MapNum
+	reply.ReduceNum = c.Mrinfo.ReduceNum
+
+	return nil
+}
 
 //
 // start a thread that listens for RPCs from worker.go
@@ -46,10 +166,12 @@ func (c *Coordinator) server() {
 // if the entire job has finished.
 //
 func (c *Coordinator) Done() bool {
-	ret := false
+	ret := true
 
 	// Your code here.
-
+	for !(c.IsMapFinished() && c.IsReduceFinished()) {
+		time.Sleep(time.Second * 1)
+	}
 
 	return ret
 }
@@ -63,7 +185,22 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
+	c.Mrinfo.MapNum = len(files)
+	c.Mrinfo.ReduceNum = nReduce
 
+	// Add Map tasks
+	c.mtx.Lock()
+	for i, filename := range files {
+		task := Task{MapTask, filename, i, UnAssigned}
+		c.MapTasks = append(c.MapTasks, task)
+	}
+
+	// Add Reduce tasks
+	for i := 0; i < nReduce; i++ {
+		task := Task{ReduceTask, "", i, UnAssigned}
+		c.ReduceTasks = append(c.ReduceTasks, task)
+	}
+	c.mtx.Unlock()
 
 	c.server()
 	return &c
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,25 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"sort"
+	"strconv"
+	"time"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
 
+// for sorting by key.
+type ByKey []KeyValue
+
+// for sorting by key.
+func (a ByKey) Len() int           { return len(a) }
+func (a ByKey) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a ByKey) Less(i, j int) bool { return a[i].Key < a[j].Key }
 
 //
 // Map functions return a slice of KeyValue.
@@ -24,7 +39,6 @@ func ihash(key string) int {
 	return int(h.Sum32() & 0x7fffffff)
 }
 
-
 //
 // main/mrworker.go calls this function.
 //
@@ -32,6 +46,30 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// Your worker implementation here.
+	// Get the number of Reduce tasks from coordinator
+	args := ArgsType{}
+	var reply MRInfo
+	ok := call("Coordinator.GetMRInfo", args, &reply)
+	if !ok {
+		log.Println("Fail to GetMRInfo, Worker func is going to  exist")
+		return
+	}
+
+	// Loop until RPC connection is broken, it normally means that the coordinator has existed.
+	for {
+		// Step1. Get a task from coordinator, get Reduce task only after all the Map tasks have
+		// been completed.
+		task := GetTask(ArgsType{})
+
+		// Step2. Execute this task and update status to coordinator
+		if task.TaskType == MapTask {
+			DoMapOperation(&task, reply.ReduceNum, mapf)
+		} else if task.TaskType == ReduceTask {
+			DoReduceOperation(&task, reply.MapNum, reducef)
+		} else {
+			log.Println("Worker: Unsupported task type")
+		}
+	}
 
 	// uncomment to send the Example RPC to the coordinator.
 	// CallExample()
@@ -65,6 +103,131 @@ func CallExample() {
 	} else {
 		fmt.Printf("call failed!\n")
 	}
+}
+
+func DoMapOperation(task *Task, nReduce int, mapf func(string, string) []KeyValue) {
+	file, err := os.Open(task.FileName)
+	if err != nil {
+		log.Fatalf("cannot open %v", task.FileName)
+	}
+	content, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Fatalf("cannot read %v", task.FileName)
+	}
+	file.Close()
+	kva := mapf(task.FileName, string(content))
+
+	// split output by the hash result
+	intermediate := make(map[int][]KeyValue)
+	for _, line := range kva {
+		index := ihash(line.Key) % nReduce
+		intermediate[index] = append(intermediate[index], line)
+	}
+
+	// open output file and write with JSON format
+	for reduceId, data := range intermediate {
+		oname := "mr-" + strconv.Itoa(task.TaskIndex) + "-" + strconv.Itoa(reduceId)
+		//log.Printf("Writing file:%v\n", oname)
+		//ofile, _ := os.Create(oname)
+		ofile, _ := ioutil.TempFile(".", "temp-")
+
+		enc := json.NewEncoder(ofile)
+		for _, kv := range data {
+			err := enc.Encode(&kv)
+			if err != nil {
+				log.Printf("enc.Encode failed, key=%v, value=%v\n", kv.Key, kv.Value)
+			}
+		}
+
+		os.Rename(ofile.Name(), oname)
+		ofile.Close()
+	}
+
+	UpdateTaskStatus(task)
+}
+
+func DoReduceOperation(task *Task, nMap int, reducef func(string, []string) string) {
+	var kva []KeyValue
+
+	// read reduce input files with JSON format
+	for i := 0; i < nMap; i++ {
+		oname := "mr-" + strconv.Itoa(i) + "-" + strconv.Itoa(task.TaskIndex)
+		//log.Printf("Reading file:%v\n", oname)
+		ofile, _ := os.Open(oname)
+
+		dec := json.NewDecoder(ofile)
+
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			kva = append(kva, kv)
+		}
+	}
+
+	sort.Sort(ByKey(kva))
+	oname := "mr-out-" + strconv.Itoa(task.TaskIndex)
+	//ofile, _ := os.Create(oname)
+	ofile, _ := ioutil.TempFile(".", "temp-")
+
+	i := 0
+	for i < len(kva) {
+		j := i + 1
+		for j < len(kva) && kva[j].Key == kva[i].Key {
+			j++
+		}
+		values := []string{}
+		for k := i; k < j; k++ {
+			values = append(values, kva[k].Value)
+		}
+		output := reducef(kva[i].Key, values)
+		fmt.Fprintf(ofile, "%v %v\n", kva[i].Key, output)
+
+		i = j
+	}
+
+	os.Rename(ofile.Name(), oname)
+	ofile.Close()
+
+	UpdateTaskStatus(task)
+}
+
+func UpdateTaskStatus(task *Task) {
+	args := TaskFinishArgs{}
+	reply := TaskFinishReply{}
+	args.TaskType = task.TaskType
+	args.TaskId = task.TaskIndex
+	ok := call("Coordinator.UpdateTaskStatus", &args, &reply)
+	if !ok {
+		if task.TaskType == MapTask {
+			log.Printf("MapTask failed! fileName=%v, taskId=%v\n", task.FileName, task.TaskIndex)
+		} else if task.TaskType == ReduceTask {
+			log.Printf("ReduceTask failed! fileName=%v, taskId=%v\n", task.FileName, task.TaskIndex)
+		}
+	}
+}
+
+// Get a task from the coordinator
+func GetTask(args ArgsType) Task {
+	reply := Task{}
+
+	for {
+		ok := call("Coordinator.AssignTask", &args, &reply)
+		if ok {
+			if reply.TaskType != NoMoreTask {
+				break
+			} else {
+				log.Println("NoMoreTask")
+			}
+		}
+		// Can not get a available task to execute, wait for a second and try to get again.
+		time.Sleep(time.Second * 1)
+	}
+
+	log.Printf("Worker: Task.TaskType: %d Task.TaskIndex %d Task.TaskStatus %d\n",
+		reply.TaskType, reply.TaskIndex, reply.TaskStatus)
+	return reply
 }
 
 //
